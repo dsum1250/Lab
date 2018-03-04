9b61dc6f58f5a6b22242e9519fcb6a355912788c
dsum1250 <37031359+dsum1250@users.noreply.github.com>
2018-03-03 19:29:44 -0800

    Update Program.cs

--- Foo/Foo/Program.cs
+++ Foo/Foo/Program.cs
@@ -10,7 +10,8 @@ namespace Foo
     {
         static void Main(string[] args)
         {
-            // Someone edited code
+            // Someone rewrote code
+            // Second line
         }
     }
 }
