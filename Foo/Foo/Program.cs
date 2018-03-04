db67b11d7eec27543b9fce927acde98e802fa94b
David <thehistorian777@hotmail.com>
2018-03-04 14:19:39 -0800

    Added Add Function.

--- Foo/Foo/Program.cs
+++ Foo/Foo/Program.cs
@@ -20,5 +20,14 @@ namespace Foo
             // Eighth Line
             // Ninth Line
         }
+
+        public int Add()
+        {
+            int x1 = 1;
+            int x2 = 2;
+            int sum = x1 + x2;
+
+            return sum;
+        }
     }
 }
